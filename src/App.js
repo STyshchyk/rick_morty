37ac80a31296ea0cb0237b6e5a634d08910d5d6f
STyshchyk <svet.tyshchyk@gmail.com>
2023-03-11 16:54:45 +0200

    feat: style cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.scss';
 import svgLogo from './UI/Logo.svg'
-import {useGetGoodsQuery} from "./store/cardApi";
 import CardList from "./Components/CardList";
 
 function App() {
-    const {data, isLoading} = useGetGoodsQuery()
     return (
         <div className="App">
             <div className={"logo"}>
                 <img src={svgLogo} alt="Logo" width={300} height={100}/>
             </div>
+            <CardList/>
         </div>
-
     );
 }
 
